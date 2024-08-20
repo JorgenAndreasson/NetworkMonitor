a2a564a211ffaf470e8fcede0b7790aa3d343b3c
Jörgen Andreasson <contractor.jorgen.andreasson@esb.ie>
2024-08-20 16:14:23 +0100

    Built NetworkMonitor

--- Sources/NetworkMonitor/NetworkMonitor.swift
+++ Sources/NetworkMonitor/NetworkMonitor.swift
@@ -1,2 +1,36 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import SwiftUI
+import Network
+
+@Observable
+public class NetworkMonitor: ObservableObject {
+    private let monitor = NWPathMonitor()
+    private let queue = DispatchQueue(label: "NWMonitor")
+    var isConnected = false
+    var isExpensive = false
+    var isConstrained = false
+    var connectionType = NWInterface.InterfaceType.other
+    
+    init() {
+        monitor.pathUpdateHandler = { path in
+            self.isConnected = path.status == .satisfied
+            self.isExpensive = path.isExpensive
+            self.isConstrained = path.isConstrained // Low Data Mode
+            
+            let connectionTypes: [NWInterface.InterfaceType] = [
+                .cellular,
+                .wifi,
+                .wiredEthernet]
+            self.connectionType = connectionTypes.first(where: path.usesInterfaceType) ?? .other
+            
+            DispatchQueue.main.async {
+                self.objectWillChange.send()
+            }
+        }
+        
+        monitor.start(queue: queue)
+    }
+    
+}
